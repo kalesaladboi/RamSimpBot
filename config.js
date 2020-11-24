d24c103a5515495a9e056d7c2119d7a771208efb
Guapo <kyle.miller.pokemonpro@gmail.com>
2020-11-24 00:21:56 -0500

    twitterbotproj

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    consumer_key:         'mtvZTDhxnab8h44gb11K0d77O',
+    consumer_secret:      'wCjRpZNs6j1ZwfcUTNbO58HqmSzrg8bowszsBgPNQcB3f2AhOS',
+    access_token:         '1326359789740453888-geiVUWf2xO03FnwY79tUOAg4bjCiNs',
+    access_token_secret:  'ZPXeWWjJ4whoMEIure7OxWpfyhdnxaIL6wtgcqsttHaQi',
+    timeout_ms:           10*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  }
+  
+  module.exports = config;
---
+++ test.js
@@ -0,0 +1,195 @@
+console.log('Ram Worship Initiate');
+
+var Twit = require( 'twit' ),
+      fs = require( 'fs' ),
+      path = require( 'path' ),
+      config = require( path.join( __dirname, 'config.js' ) );
+
+var config = require('./config.js');
+
+var T = new Twit( config );
+
+var stream = T.stream('statuses/filter', { track: '@ram_simp' });
+
+stream.on( 'tweet', tweetEvent)
+
+
+function tweetEvent(eventMsg) {
+    var json = JSON.stringify(eventMsg,null,2);
+    fs.writeFileSync("tweet.json" , json);
+    var replyto = eventMsg.in_reply_to_screen_name;
+    //var tweet_id = eventMsg.id_str;
+    
+    //console.log(eventMsg.id)
+    //console.log(eventMsg.text)
+    //console.log(eventMsg.in_reply_to_screen_name)
+    //console.log(eventMsg.in_reply_to_user_id_str)
+    //console.log(eventMsg.in_reply_to_user_id)
+    console.log(eventMsg.user)
+
+    if (replyto === 'ram_simp') {
+      tweetIt(eventMsg); 
+    }
+  }
+
+function tweetIt(eventMsg) {
+    b64content = fs.readFileSync( imagePath, { encoding: 'base64' } );
+    const imagePath = path.join(__dirname, "Cards" , rarity , answer );
+  var mediaArray = [];
+
+  const posts = new Promise((res,rej) => {
+    T.post("media/upload",{media_data: b64content}, function (err, data, response) {
+     mediaArray.push(data.media_id_string)
+      console.log(mediaArray)
+      console.log(rarity)
+      console.log( 'picking a random common...' );
+      var rarity = Math.ceil( Math.random() * 100 )
+  
+      if ( rarity >= 0 ) {
+  
+        var rarity = 'common'
+      }
+      console.log(rarity)
+  
+      filenames = fs.readdirSync(path.join(__dirname, "Cards" , rarity))
+    
+      var outcome = Math.ceil( Math.random() * Object.keys(filenames).length)
+    
+      var answer = filenames[outcome]
+  
+      console.log(filenames[outcome])
+  
+      const imagePath = path.join(__dirname, "Cards" , rarity , answer );
+  
+      b64content = fs.readFileSync( imagePath, { encoding: 'base64' } );
+  
+    console.log( 'uploading an image...' );
+  res();
+  })
+  }).then(() => {
+  return new Promise((res,rej) => {
+   T.post("media/upload",{media_data: b64content}, function (err, data, response) {
+     mediaArray.push(data.media_id_string)
+      console.log(mediaArray)
+      console.log(rarity)
+      console.log( 'picking a random common...' );
+      var rarity = Math.ceil( Math.random() * 100 )
+  
+      if ( rarity >= 0 ) {
+  
+        var rarity = 'common'
+      }
+      console.log(rarity)
+  
+      filenames = fs.readdirSync(path.join(__dirname, "Cards" , rarity))
+    
+      var outcome = Math.ceil( Math.random() * Object.keys(filenames).length)
+    
+      var answer = filenames[outcome]
+  
+      console.log(filenames[outcome])
+  
+      const imagePath = path.join(__dirname, "Cards" , rarity , answer );
+  
+      b64content = fs.readFileSync( imagePath, { encoding: 'base64' } );
+  
+    console.log( 'uploading an image...' );
+  res();
+  })
+  }).then( (res,rej) => {
+  return new Promise((res,rej) => {
+   T.post("media/upload",{media_data: b64content}, function (err, data, response) {
+      mediaArray.push(data.media_id_string)
+      console.log(mediaArray)
+      console.log(rarity)
+      console.log( 'picking a random image...' );
+      var rarity = Math.ceil( Math.random() * 100 )
+  
+        if ( rarity > 1 || rarity < 5){
+      
+        var rarity = 'rare'
+    
+      } else if( rarity > 5) {
+      
+        var rarity = 'uncommon'
+
+      }
+      console.log(rarity)
+  
+      filenames = fs.readdirSync(path.join(__dirname, "Cards" , rarity))
+    
+      var outcome = Math.ceil( Math.random() * Object.keys(filenames).length)
+    
+      var answer = filenames[outcome]
+  
+      console.log(filenames[outcome])
+  
+      const imagePath = path.join(__dirname, "Cards" , rarity , answer );
+  
+      b64content = fs.readFileSync( imagePath, { encoding: 'base64' } );
+  
+    console.log( 'uploading an image...' );
+  res();
+  })
+  }).then(() => {
+  return new Promise((res,rej) => {
+    T.post("media/upload",{media_data: b64content}, function (err, data, response) {
+      mediaArray.push(data.media_id_string)
+      console.log(mediaArray)
+      console.log(rarity)
+      console.log( 'picking a random image...' );
+      var rarity = Math.ceil( Math.random() * 100 )
+  
+      if ( rarity < 1 ) {
+  
+        var rarity = 'epic'
+    
+      } else if ( rarity > 1 ){
+      
+        var rarity = 'rare'
+      }
+      console.log(rarity)
+  
+      filenames = fs.readdirSync(path.join(__dirname, "Cards" , rarity))
+    
+      var outcome = Math.ceil( Math.random() * Object.keys(filenames).length)
+    
+      var answer = filenames[outcome]
+  
+      console.log(filenames[outcome])
+  
+      const imagePath = path.join(__dirname, "Cards" , rarity , answer );
+  
+      b64content = fs.readFileSync( imagePath, { encoding: 'base64' } );
+  
+    console.log( 'uploading an image...' );
+  res();
+  })
+}).then(() => {console.log('dissapointment') 
+var mediaIdStr = mediaArray.join(",")
+var altText = "Ram Me Harder"
+var meta_params = { media_id: [mediaArray] , alt_text: { text: altText } }
+        return new Promise((res,rej) => {
+T.post('media/metadata/create', meta_params, function (err, data, response) {
+   if (!err) {
+     var params = { status: `@${eventMsg.user.screen_name} Get Rammed` , media_id: mediaIdStr , in_reply_to_status_id: eventMsg.id_str }
+T.post( 'statuses/update', params , function( err, data, response) {
+          if (err){
+          console.log( 'error:', err );
+          }
+       else{
+           console.log( 'posted an image!' );
+           res(response);
+         }
+      }
+  );
+
+  
+};
+})
+}).then(result => console.log(result)).catch(err => console.log(err));
+})
+})
+})
+})
+}
